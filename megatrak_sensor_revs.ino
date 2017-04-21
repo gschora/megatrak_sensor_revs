c1a6b57cf444e737728a6ce8c10fb877e0e4b039
hannes <hannes.dummy@putz-transporte.at>
2017-04-21 14:01:57 +0200

    added 3 pins for revision counter added serial commands for setting eeprom settings divider,precision,revMode,diameter

--- megatrak_sensor_revs.ino
+++ megatrak_sensor_revs.ino
@@ -12,6 +12,20 @@
 
 #define EEPROM_NODE_ADDRESS 0
 #define EEPROM_SERVER_ADDRESS 1
+#define EEPROM_DIV1_ADDRESS 2
+#define EEPROM_DIV2_ADDRESS 3
+#define EEPROM_DIV3_ADDRESS 4
+#define EEPROM_MOD1_ADDRESS 5
+#define EEPROM_MOD2_ADDRESS 6
+#define EEPROM_MOD3_ADDRESS 7
+#define EEPROM_PREC1_ADDRESS 8
+#define EEPROM_PREC2_ADDRESS 9
+#define EEPROM_PREC3_ADDRESS 10
+#define EEPROM_DIAM1_ADDRESS 11
+#define EEPROM_DIAM2_ADDRESS 12
+#define EEPROM_DIAM3_ADDRESS 13
+
+
 uint8_t SERVER_ADDRESS = EEPROM.read(EEPROM_SERVER_ADDRESS);
 uint8_t NODE_ADDRESS = EEPROM.read(EEPROM_NODE_ADDRESS);
 
@@ -25,6 +39,36 @@ uint8_t msg[RH_RF69_MAX_MESSAGE_LEN];
 
 unsigned long time = 0;
 
+const uint8_t revPin1 = 10;
+unsigned long revTimeOld1 = 0;
+unsigned long revTimeNew1 = 0;
+unsigned long revTimeDiff1 = 0;
+uint16_t revs1 = 0;
+uint8_t divider1 = EEPROM.read(EEPROM_DIV1_ADDRESS); //Divider for one revision
+uint8_t precision1 = EEPROM.read(EEPROM_PREC1_ADDRESS); //precision used for calculating revs per min
+uint8_t revMode1 = EEPROM.read(EEPROM_MOD1_ADDRESS); // Mode 0 = umin, 1 = kmh
+uint8_t diam1 = EEPROM.read(EEPROM_DIAM1_ADDRESS);
+
+const uint8_t revPin2 = 3;
+unsigned long revTimeOld2 = 0;
+unsigned long revTimeNew2 = 0;
+unsigned long revTimeDiff2 = 0;
+uint16_t revs2 = 0;
+uint8_t divider2 = EEPROM.read(EEPROM_DIV2_ADDRESS); //Divider for one revision
+uint8_t precision2 = EEPROM.read(EEPROM_PREC2_ADDRESS); //precision used for calculating revs per min
+uint8_t revMode2 = EEPROM.read(EEPROM_MOD2_ADDRESS); // Mode 0 = umin, 1 = kmh
+uint8_t diam2 = EEPROM.read(EEPROM_DIAM2_ADDRESS);
+
+const uint8_t revPin3 = 4;
+unsigned long revTimeOld3 = 0;
+unsigned long revTimeNew3 = 0;
+unsigned long revTimeDiff3 = 0;
+uint16_t revs3 = 0;
+uint8_t divider3 = EEPROM.read(EEPROM_DIV3_ADDRESS); //Divider for one revision
+uint8_t precision3 = EEPROM.read(EEPROM_PREC3_ADDRESS); //precision used for calculating revs per min
+uint8_t revMode3 = EEPROM.read(EEPROM_MOD3_ADDRESS); // Mode 0 = umin, 1 = kmh
+uint8_t diam3 = EEPROM.read(EEPROM_DIAM3_ADDRESS);
+
 void setup() {
 	Serial.begin(57600);
 	while (!Serial && (millis() <= 8000));
@@ -36,6 +80,10 @@ void setup() {
 	cmd.addCommand("ssa", sc_setServerAddress); //set server address
 	cmd.addCommand("cfg", sc_printCfg); //print the current config
 	cmd.addCommand("scn", sc_sndCmdNode); //send a command to a node over rf
+	cmd.addCommand("srd", sc_setRevDivider);
+	cmd.addCommand("srp", sc_setRevPrecision);
+	cmd.addCommand("srm", sc_setRevMode);
+	cmd.addCommand("srdia", sc_setRevDiameter);
 	//###################################################
 
 	if (!manager.init())
@@ -48,6 +96,13 @@ void setup() {
 	// If you are using a high power RF69, you *must* set a Tx power in the
 	// range 14 to 20 like this:
 	rf69.setTxPower(14);
+
+	pinMode(revPin1, INPUT);
+	attachInterrupt(revPin1, countRevs1, FALLING);
+	pinMode(revPin2, INPUT);
+	attachInterrupt(revPin2, countRevs2, FALLING);
+	pinMode(revPin3, INPUT);
+	attachInterrupt(revPin3, countRevs3, FALLING);
 }
 
 uint8_t from;
@@ -122,8 +177,10 @@ void parseSrvCmd() {
 				sendOKMsg();
 				setEEPROMNodeAddress(atoi((char*)addr));
 			}
-					
 		}
+//TODO command for setting server address
+		//TODO command for setting mode
+		//todo command for setting devider
 	}
 
 }
@@ -227,4 +284,288 @@ void sc_printCfg() {
 	Serial.println(getEEPROMNodeAddress());
 	Serial.print("server address: ");
 	Serial.println(getEEPROMServerAddress());
+
+	Serial.print("divider: ");
+	Serial.print(divider1);
+	Serial.print("|");
+	Serial.print(divider2);
+	Serial.print("|");
+	Serial.println(divider3);
+
+	Serial.print("precision: ");
+	Serial.print(precision1);
+	Serial.print("|");
+	Serial.print(precision2);
+	Serial.print("|");
+	Serial.println(precision3);
+
+	Serial.print("revMode: ");
+	Serial.print(revMode1);
+	Serial.print("|");
+	Serial.print(revMode2);
+	Serial.print("|");
+	Serial.println(revMode3);
+
+	Serial.print("diameter: ");
+	Serial.print(diam1);
+	Serial.print("|");
+	Serial.print(diam2);
+	Serial.print("|");
+	Serial.println(diam3);
+
+}
+
+void sc_setRevDiameter() {
+	uint8_t revNr;
+	uint16_t len;
+	char *arg;
+	arg = cmd.next();
+	if (arg != NULL) {
+		revNr = atoi(arg);    // Converts a char string to an integer
+	}
+	else {
+		Serial.println("srdia - revNr is missing! aborting...");
+		return;
+	}
+	arg = cmd.next();
+	if (arg != NULL) {
+		len = atoi(arg);    // Converts a char string to an integer
+	}
+	else {
+		Serial.println("srdia - diameter is missing! aborting...");
+		return;
+	}
+	setEEPROMRevLength(revNr, len); 
+}
+void setEEPROMRevLength(uint8_t revNr, uint8_t dia) {
+	uint16_t olen;
+	Serial.print("setting revdiameter ");
+	switch (revNr) {
+	case 1:
+		Serial.print(revNr);
+		Serial.print(" from ");
+		olen = (int)EEPROM.read(EEPROM_DIAM1_ADDRESS); //diameter in cm used for calculating kmh
+		Serial.print(olen, DEC);
+		Serial.print(" to ");
+		Serial.println(dia);
+		EEPROM.write(EEPROM_DIAM1_ADDRESS, dia);
+		diam1 = dia;
+		break;
+	case 2:
+		Serial.print(revNr);
+		Serial.print(" from ");
+		olen = (int)EEPROM.read(EEPROM_DIAM2_ADDRESS); //diameter in cm used for calculating kmh
+		Serial.print(olen, DEC);
+		Serial.print(" to ");
+		Serial.println(dia);
+		EEPROM.write(EEPROM_DIAM2_ADDRESS, dia);
+		diam2 = dia;
+		break;
+	case 3:
+		Serial.print(revNr);
+		Serial.print(" from ");
+		olen = (int)EEPROM.read(EEPROM_DIAM3_ADDRESS); //diameter in cm used for calculating kmh
+		Serial.print(olen, DEC);
+		Serial.print(" to ");
+		Serial.println(dia);
+		EEPROM.write(EEPROM_DIAM3_ADDRESS, dia);
+		diam3 = dia;
+		break;
+	}
+	Serial.println("done!");
+}
+
+void sc_setRevMode() {
+	uint8_t revNr;
+	uint8_t mod;
+	char *arg;
+	arg = cmd.next();
+	if (arg != NULL) {
+		revNr = atoi(arg);    // Converts a char string to an integer
+	}
+	else {
+		Serial.println("srm - revNr is missing! aborting...");
+		return;
+	}
+	arg = cmd.next();
+	if (arg != NULL) {
+		mod = atoi(arg);    // Converts a char string to an integer
+	}
+	else {
+		Serial.println("srm - mode is missing! aborting...");
+		return;
+	}
+	setEEPROMRevMode(revNr, mod);
+}
+void setEEPROMRevMode(uint8_t revNr, uint8_t rmode) {
+	Serial.print("setting revmode ");
+	switch (revNr) {
+	case 1:
+		Serial.print(revNr);
+		Serial.print(" from ");
+		Serial.print(EEPROM.read(EEPROM_MOD1_ADDRESS), DEC);
+		Serial.print(" to ");
+		Serial.println(rmode);
+		EEPROM.write(EEPROM_MOD1_ADDRESS, rmode);
+		revMode1 = rmode;
+		break;
+	case 2:
+		Serial.print(revNr);
+		Serial.print(" from ");
+		Serial.print(EEPROM.read(EEPROM_MOD2_ADDRESS), DEC);
+		Serial.print(" to ");
+		Serial.println(rmode);
+		EEPROM.write(EEPROM_MOD2_ADDRESS, rmode);
+		revMode2 = rmode;
+		break;
+	case 3:
+		Serial.print(revNr);
+		Serial.print(" from ");
+		Serial.print(EEPROM.read(EEPROM_MOD3_ADDRESS), DEC);
+		Serial.print(" to ");
+		Serial.println(rmode);
+		EEPROM.write(EEPROM_MOD3_ADDRESS, rmode);
+		revMode3 = rmode;
+		break;
+	}
+	Serial.println("done!");
+}
+void sc_setRevPrecision() {
+	uint8_t revNr;
+	uint8_t prec;
+	char *arg;
+	arg = cmd.next();
+	if (arg != NULL) {
+		revNr = atoi(arg);    // Converts a char string to an integer
+	}
+	else {
+		Serial.println("srp - revNr is missing! aborting...");
+		return;
+	}
+	arg = cmd.next();
+	if (arg != NULL) {
+		prec = atoi(arg);    // Converts a char string to an integer
+	}
+	else {
+		Serial.println("srp - precision is missing! aborting...");
+		return;
+	}
+	setEEPROMRevPrecision(revNr, prec);
+}
+void setEEPROMRevPrecision(uint8_t revNr, uint8_t prec) {
+	Serial.print("setting precision ");
+	switch (revNr) {
+	case 1:
+		Serial.print(revNr);
+		Serial.print(" from ");
+		Serial.print(EEPROM.read(EEPROM_PREC1_ADDRESS), DEC);
+		Serial.print(" to ");
+		Serial.println(prec);
+		EEPROM.write(EEPROM_PREC1_ADDRESS, prec);
+		precision1 = prec;
+		break;
+	case 2:
+		Serial.print(revNr);
+		Serial.print(" from ");
+		Serial.print(EEPROM.read(EEPROM_PREC2_ADDRESS), DEC);
+		Serial.print(" to ");
+		Serial.println(prec);
+		EEPROM.write(EEPROM_PREC2_ADDRESS, prec);
+		precision2 = prec;
+		break;
+	case 3:
+		Serial.print(revNr);
+		Serial.print(" from ");
+		Serial.print(EEPROM.read(EEPROM_PREC3_ADDRESS), DEC);
+		Serial.print(" to ");
+		Serial.println(prec);
+		EEPROM.write(EEPROM_PREC3_ADDRESS, prec);
+		precision3 = prec;
+		break;
+	}
+	Serial.println("done!");
+}
+
+void sc_setRevDivider() {
+	uint8_t revNr;
+	uint8_t div;
+	char *arg;
+	arg = cmd.next();
+	if (arg != NULL) {
+		revNr = atoi(arg);    // Converts a char string to an integer
+	}
+	else {
+		Serial.println("srd - revNr is missing! aborting...");
+		return;
+	}
+	arg = cmd.next();
+	if (arg != NULL) {
+		div = atoi(arg);    // Converts a char string to an integer
+	}
+	else {
+		Serial.println("srd - divider is missing! aborting...");
+		return;
+	}
+	setEEPROMRevDivider(revNr, div);
+}
+void setEEPROMRevDivider(uint8_t revNr, uint8_t div) {
+	Serial.print("setting divider ");
+	switch (revNr) {
+	case 1:
+		Serial.print(revNr);
+		Serial.print(" from ");
+		Serial.print(EEPROM.read(EEPROM_DIV1_ADDRESS), DEC);
+		Serial.print(" to ");
+		Serial.println(div);
+		EEPROM.write(EEPROM_DIV1_ADDRESS, div);
+		divider1 = div;
+		break;
+	case 2:
+		Serial.print(revNr);
+		Serial.print(" from ");
+		Serial.print(EEPROM.read(EEPROM_DIV2_ADDRESS), DEC);
+		Serial.print(" to ");
+		Serial.println(div);
+		EEPROM.write(EEPROM_DIV2_ADDRESS, div);
+		divider2 = div;
+		break;
+	case 3:
+		Serial.print(revNr);
+		Serial.print(" from ");
+		Serial.print(EEPROM.read(EEPROM_DIV3_ADDRESS), DEC);
+		Serial.print(" to ");
+		Serial.println(div);
+		EEPROM.write(EEPROM_DIV3_ADDRESS, div);
+		divider3 = div;
+		break;
+	}
+	Serial.println("done!");
+}
+
+void countRevs1() {
+	revTimeNew1 = millis();
+	revTimeDiff1 = revTimeNew1 - revTimeOld1;
+	revs1 = 1000 * 60 / revTimeDiff1 / divider1 / precision1 * precision1;
+	revTimeOld1 = revTimeNew1;
+
+	Serial.print("revs1: ");
+	Serial.println(revs1);
+}
+
+void countRevs2() {
+	revTimeNew2 = millis();
+	revTimeDiff2 = revTimeNew2 - revTimeOld2;
+	revs2 = 1000 * 60 / revTimeDiff2 / divider2 / precision2 * precision2;
+	revTimeOld2 = revTimeNew2;
+	Serial.print("revs2: ");
+	Serial.println(revs2);
+}
+
+void countRevs3() {
+	revTimeNew3 = millis();
+	revTimeDiff3 = revTimeNew3 - revTimeOld3;
+	revs3 = 1000 * 60 / revTimeDiff3 / divider3 / precision3 * precision3;
+	revTimeOld3 = revTimeNew3;
+	Serial.print("revs3: ");
+	Serial.println(revs3);
 }
